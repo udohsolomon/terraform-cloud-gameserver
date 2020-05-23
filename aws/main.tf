50b6b3fef04e439f36efc7cb467f7a5a2992ca97
udohsolomon <solomonlazio@gmail.com>
2020-05-22 17:07:54 -0700

    added autoscaling to cluster

--- aws/main.tf
+++ aws/main.tf
@@ -91,6 +91,102 @@ resource "aws_cloudwatch_log_group" "log_group" {
 
 data "aws_region" "current" {}
 
+
+#------------------------------------------------------------------------------
+# ECS Service Autoscaling
+#------------------------------------------------------------------------------
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity        = 3
+  min_capacity        = 1
+  resource_id         = "service/${aws_ecs_cluster.ecs_cluster}/${aws_ecs_service.ecs_service}"
+  scalable_dimension  = "ecs:service:DesiredCount"
+  service_namespace   = "ecs"
+  
+}
+
+# Automatically scale capacity up by one
+resource "aws_appautoscaling_policy" "scale_up_ecs_policy" {
+  name                = "${var.namespace}-scale_up"
+  policy_type         = "StepScaling"
+  resource_id         = "${aws_appautoscaling_target.ecs_target.resource_id}"
+  scalable_dimension  = "${aws_appautoscaling_target.ecs_target.scalable_dimension}"
+  service_namespace   = "${aws_appautoscaling_target.ecs_target.service_namespace}"
+
+  step_scaling_policy_configuration {
+    adjustment_type   = "ChangeInCapacity"
+    cooldown          = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+
+  depends_on    = [aws_appautoscaling_target.ecs_target]
+  
+}
+# Automatically scale capacity down by one
+resource "aws_appautoscaling_policy" "scale_down_ecs_policy" {
+  name                = "${var.namespace}-scale_down"
+  policy_type         = "StepScaling"
+  resource_id         = "${aws_appautoscaling_target.ecs_target.resource_id}"
+  scalable_dimension  = "${aws_appautoscaling_target.ecs_target.scalable_dimension}"
+  service_namespace   = "${aws_appautoscaling_target.ecs_target.service_namespace}"
+
+  step_scaling_policy_configuration {
+    adjustment_type   = "ChangeInCapacity"
+    cooldown          = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+
+  depends_on    = [aws_appautoscaling_target.ecs_target]
+  
+}
+
+# CloudWatch alarm that triggers the autoscaling up policy
+resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
+  alarm_name          = "${var.namespace}-cpu_utilization_high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "85"
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.ecs_cluster
+    ServiceName = aws_ecs_service.ecs_service
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.scale_up_ecs_policy.arn]
+}
+
+# CloudWatch alarm that triggers the autoscaling down policy
+resource "aws_cloudwatch_metric_alarm" "service_cpu_low" {
+  alarm_name          = "${var.namespace}-cpu_utilization_low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "10"
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.ecs_cluster
+    ServiceName = aws_ecs_service.ecs_service
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.scale_down_ecs_policy.arn]
+}
+
 #------------------------------------------------------------------------------
 # IAM Role & Policy for COP Service
 #------------------------------------------------------------------------------
